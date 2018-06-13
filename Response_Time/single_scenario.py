bfef555a27dda66e48a89aed0ef073fc33a3073b
ME-glitch <yuupvengelshoven@gmail.com>
2018-06-13 17:41:15 +0200

    scenario may be working? need to test it on the servers

--- Response_Time/single_scenario.py
+++ Response_Time/single_scenario.py
@@ -1,14 +1,39 @@
 from paramiko import SSHClient
 
-def start_iperf3_server(server_ip):
-
+def make_ssh_connection(server_ip):
 	## Make ssh connection
 	port = 22
 	ssh = SSHClient()
 	ssh.load_system_host_keys()
 	ssh.connect(server_ip, port, username="nas", password="naslab")
+	return ssh
+
+def start_iperf3_server(server_ip):
+
+	ssh = make_ssh_connection(server_ip)
 
 	## Start iperf3 on the receiving server
 	ssh.exec_command("iperf3 -s")
 
+def start_iperf3_client(client_ip, server_ip)
+	ssh = make_ssh_connection(client_ip)
+
+	## Start iperf3 test
+	stdin, stdout, stderr = ssh.exec_command("iperf3 -c " + server_ip + " -i1 -t60")
+	print(stdout)
+
+#node 15
 start_iperf3_server("10.10.2.15")
+
+# node 7 connects to node 15
+start_iperf3_client("10.10.2.7","172.16.5.1")
+
+
+#node 6
+start_iperf3_server("10.10.2.6")
+
+#node 5 connects to node 6
+start_iperf3_client("10.10.2.5", "172.16.1.1")
+
+
+
