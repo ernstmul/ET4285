bbccd48f7f89daac203ea1284d75aef7aa969891
root <root@node04.local>
2018-06-13 12:46:15 +0200

    fixed a small bug in start_iperf3_server. It is able to connect to a server and start the iperf3

--- Response_Time/single_scenario.py
+++ Response_Time/single_scenario.py
@@ -3,12 +3,12 @@ from paramiko import SSHClient
 def start_iperf3_server(server_ip):
 
 	## Make ssh connection
-	port = 2
+	port = 22
 	ssh = SSHClient()
-	ssh.load_system_keys()
+	ssh.load_system_host_keys()
 	ssh.connect(server_ip, port, username="nas", password="naslab")
 
-	## 
+	## Start iperf3 on the receiving server
 	ssh.exec_command("iperf3 -s")
 
-start_iperf3_server("10.10.2.5")
+start_iperf3_server("10.10.2.15")
