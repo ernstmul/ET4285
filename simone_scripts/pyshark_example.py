12985805e09b370531d6d52d724b6cd2a7cdb752
nas <nas@node18.local>
2018-06-17 22:34:13 +0200

    Update script/csv

--- simone_scripts/pyshark_example.py
+++ simone_scripts/pyshark_example.py
@@ -9,8 +9,8 @@ except:
     t_cap = 30 #capture time in seconds
 
 cap = pyshark.LiveCapture(interface='eth1',
-        output_file="/home/nas/Simone_scripts/out/capture.pcap")#,
-        #bpf_filter='udp')
+        output_file="/home/nas/Simone_scripts/out/capture.pcap", 
+        bpf_filter='udp')
 
 cap.sniff(timeout=t_cap)
 print(cap); #print summary of capture
@@ -21,7 +21,7 @@ packet_list = []
 packet_element = [];
 
 for i in range(0,count-1):
-    packet_element = [cap[i].sniff_timestamp, 0]
+    packet_element = [cap[i].sniff_timestamp, cap[i]['ip'].src, cap[i]['ip'].dst]
     packet_list.append(packet_element)
     #packet_list.append(cap[i].sniff_timestamp)
 
